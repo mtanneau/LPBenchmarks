b526cdeea50ca1d0e6dbe8d36ca8550f041c8fd1
mtanneau <mathieu.tanneau@gmail.com>
2020-04-23 11:26:47 -0400

    Tulip script

---
+++ src/tulip.jl
@@ -0,0 +1,34 @@
+using LinearAlgebra
+
+using TimerOutputs
+import Tulip
+
+"""
+    run_tulip
+
+Run benchmark for Tulip.
+"""
+function run_tulip(fname::String)
+    timer = TimerOutput()
+    BLAS.set_num_threads(1)
+
+    m = Tulip.Model{Float64}()
+    m.params.OutputLevel = 1
+
+    # Read file
+    @timeit timer "Read" Tulip.load_problem!(m, fname)
+
+    # Solve instance
+    @timeit timer "Solve" Tulip.optimize!(m)
+
+    # TODO: recover and extract primal solution
+
+    # Done.
+    return timer
+end
+
+if abspath(PROGRAM_FILE) == @__FILE__
+    t = run_tulip(ARGS[1])
+    display(t)
+    println()
+end
