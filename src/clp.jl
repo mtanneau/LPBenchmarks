b6c4ebeb9b3aa1fa2b62a0f9e32e0b7f8310a862
mtanneau <mathieu.tanneau@gmail.com>
2020-04-24 08:48:12 -0400

    Update Clp script

--- src/clp.jl
+++ src/clp.jl
@@ -5,14 +5,37 @@ function run_clp(fname::String)
     timer = TimerOutput()
 
     clp = Clp.ClpCInterface.ClpModel()
+    options = Clp.ClpCInterface.ClpSolve()
+
+    Clp.ClpCInterface.set_solve_type(options, 4)  # barrier, no crossover
 
     @timeit timer "Read" Clp.ClpCInterface.read_mps(clp, fname)
 
     # TODO: remove integer variables if any
+    @timeit timer "Solve" Clp.ClpCInterface.initial_solve_with_options(clp, options)
 
-    @timeit timer "Solve" Clp.ClpCInterface.initial_barrier_no_cross_solve(clp)
+    # TODO: extract solver status and solution
+    st = Clp.status(clp)
+    print("\n\nClp status: ")
+    if st == 0
+        println("0 - optimal")
+    elseif st == 1
+        println("1 - primal infeasible")
+    elseif st == 2
+        println("2 - dual infeasible")
+    elseif st == 3
+        println("3 - stopped on iterations etc")
+    elseif st == 4
+        println("4 - stopped due to errors")
+    else
+        println("Expected integer in [0, 4] but got $st")
+    end
+
 
     # Display timing info
+    flush(stdout)
+    flush(stderr)
+    sleep(0.10)
     println()
     display(timer)
     println()
