074464f69366a67d335dd931a032eff25e4d398e
mtanneau <mathieu.tanneau@gmail.com>
2020-04-28 16:31:07 -0400

    Add CPLEX and Gurobi slurm scripts

--- scripts/run_cplex.sh
+++ scripts/run_cplex.sh
@@ -15,4 +15,4 @@ LPBDIR="/home/mtanneau/projects/def-alodi/mtanneau/LPBenchmarks"
 mapfile -t inst < $LPBDIR/scripts/plato.txt
 
 # Run Clp on each instance
-julia --sysimage=$LPBDIR/JuliaLP.so --project=$LPBDIR $LPBDIR/src/mosek.jl $LPBDIR/dat/plato/${inst[${SLURM_ARRAY_TASK_ID}-1]} > $LPBDIR/logs/mosek/${inst[${SLURM_ARRAY_TASK_ID}-1]}.msk 2>&1
+julia --sysimage=$LPBDIR/JuliaLP.so --project=$LPBDIR $LPBDIR/src/cplex.jl $LPBDIR/dat/plato/${inst[${SLURM_ARRAY_TASK_ID}-1]} > $LPBDIR/logs/cplex/${inst[${SLURM_ARRAY_TASK_ID}-1]}.cpx 2>&1
--- snoop.jl
+++ snoop.jl
@@ -1,4 +1,5 @@
 include(joinpath(@__DIR__, "src/clp.jl"))
+include(joinpath(@__DIR__, "src/cplex.jl"))
 include(joinpath(@__DIR__, "src/ecos.jl"))
 include(joinpath(@__DIR__, "src/glpk.jl"))
 include(joinpath(@__DIR__, "src/gurobi.jl"))
@@ -9,6 +10,7 @@ for finst in ["afiro.mps", "25fv47.mps", "pds-02.mps", "qap8.mps", "pilot.mps"]
     fname = joinpath(@__DIR__, "dat/netlib", finst)
 
     run_clp(fname)
+    run_cplex(fname)
     run_ecos(fname)
     run_glpk(fname)
     run_gurobi(fname)
